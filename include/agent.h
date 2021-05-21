ff396a97f18b3bd459dc6cdfd9a1fce04d3595b3
Oleg Zilman <zilmano@gmail.com>
2021-05-21 17:51:38 -0500

    work in progress

--- include/agent.h
+++ include/agent.h
@@ -1,31 +1,50 @@
 #include "ros/ros.h"
 #include <string>
 #include "std_msgs/String.h"
+#include <stdlib.h>
 
 const std::string plan_topic = "plan_topic";
 
 class Agent {
+private:
+	static void plan_comm_callback(const distributed_mapf::PathMsg& msg) {
+		if (agent_id_ != msg.sender_id) {
+			ROS_INFO("I heard a plan message from agent [%s]", std::to_string(msg.sender_id).c_str());
+		}
+	}
+
 public:
-	Agent(ros::NodeHandle *n): n_(n){}
+	Agent(ros::NodeHandle *n): n_(n) {}
 
     void initComm() {
-    	path_publish_ = n_->advertise<distributed_mapf::PathMsg>(plan_topic, 1000);
+    	plan_publish_ = n_->advertise<distributed_mapf::PathMsg>(plan_topic, 1000);
     	test_publish_ = n_->advertise<std_msgs::String>("chatter", 1000);
+
+    	plan_subscribe_ = n_->subscribe(plan_topic, 1000, Agent::plan_comm_callback);
     }
 
-    void publishPath() {
-
+    void publishPlan(const distributed_mapf::PathMsg& msg) {
+    	plan_publish_.publish(msg);	
     }
 
     void publishTest(const std_msgs::String& msg) {
     	test_publish_.publish(msg);
     }
 
+    static void setAgentId(unsigned int id) {
+    	agent_id_ = id;
+    }
+
+
+
 private:
 	ros::NodeHandle* n_;
-	ros::Publisher path_publish_;
-	ros::Subscriber path_subscribe_;
+	ros::Publisher plan_publish_;
+	ros::Subscriber plan_subscribe_;
 	ros::Publisher test_publish_;
+
+	static unsigned int agent_id_;
+	//vector clock 
 
 
 };
--- src/agent_main.cc
+++ src/agent_main.cc
@@ -2,47 +2,60 @@
 //#include "std_msgs/String.h"
 
 #include <sstream>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "distributed_mapf/Vertex.h"
 #include "distributed_mapf/PathMsg.h"
 #include "../include/agent.h"  
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+  
+  pid_t pid = getpid();
+  std::stringstream ss;
+  ss << "agent_" << pid;
+  std::string node_name = ss.str();
+
+  ros::init(argc, argv, node_name);
   ros::NodeHandle n;
   Agent agent(&n);
+  
 
   ros::Rate loop_rate(10);
   agent.initComm();
    
-     /**
-     * A count of how many messages we have sent. This is used to create
-     * a unique string for each message.
-     */
-     int count = 0;
-     while (ros::ok())
-     {
-       /**
-        * This is a message object. You stuff it with data, and then publish it.
-        */
-       std_msgs::String msg;
-   
-       std::stringstream ss;
-       ss << "hello world " << count;
-       msg.data = ss.str();
-   
-       ROS_INFO("%s", msg.data.c_str());
-   
-       /**
-        * The publish() function is how you send messages. The parameter
-        * is the message object. The type of this object must agree with the type
-        * given as a template parameter to the advertise<>() call, as was done
-        * in the constructor above.
-       */
-       agent.publishTest(msg);
-  
-       ros::spinOnce();
-  
+   /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+  int count = 0;
+  while (ros::ok()) {
+      /**
+      * This is a message object. You stuff it with data, and then publish it.
+      */
+      
+      /*std_msgs::String msg;
+      std::stringstream ss;
+      ss << "hello world " << count;
+      msg.data = ss.str();
+
+      ROS_INFO("%s", msg.data.c_str());*/
+      distributed_mapf::PathMsg msg;
+      msg.sender_id = (unsigned int)pid;
+
+      /**
+      * The publish() function is how you send messages. The parameter
+      * is the message object. The type of this object must agree with the type
+      * given as a template parameter to the advertise<>() call, as was done
+      * in the constructor above.
+      */
+      //agent.publishTest(msg);
+      agent.publishPlan(msg);
+
+
+      ros::spinOnce();
+
       loop_rate.sleep();
       ++count;
   }
