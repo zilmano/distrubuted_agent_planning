bad5994172842d0c942933e5faa4a42ba6a8fe13
shoaibCS <19100080@lums.edu.pk>
2021-05-24 21:27:58 -0500

    minor fix

--- src/agent.cc
+++ src/agent.cc
@@ -12,7 +12,7 @@ void Agent::plan_subscribe_callback(const distributed_mapf::PathMsg& msg) {
 		if(random_num%10!=0){
 			// Delay the packet if the random number is multple of 3.
 
-			if (random_num%10!=0)
+			if (random_num%3==0)
 				sleep(2);
 			
 			ROS_INFO("Agent [%s]:I heard a plan message from agent [%s]",std::to_string(Agent::agent_id_).c_str(), std::to_string(msg.sender_id).c_str());
