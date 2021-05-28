bf340383de0e65e63d3d8c522f42182a489e8cff
shoaibCS <19100080@lums.edu.pk>
2021-05-28 02:29:08 -0700

    made delay time randomized

--- src/agent.cc
+++ src/agent.cc
@@ -7,12 +7,14 @@ void Agent::plan_subscribe_callback(const distributed_mapf::PathMsg& msg) {
 		// Drop the packet if the random number is not multiple of ten
 
 		srand (time(NULL));
-		int random_num = rand() % 100 + 1;
+		int first_random_num = rand() % 100 + 1;
 		
-		if(random_num%10!=0){
+		if(first_random_num%10!=0){
 			// Delay the packet if the random number is multple of 3.
 
-			if (random_num%3==0)
+			int second_random_num = rand() % 90 + 1;
+
+			if (second_random_num%3==0)
 				sleep(2);
 			
 			ROS_INFO("Agent [%s]:I heard a plan message from agent [%s]",std::to_string(Agent::agent_id_).c_str(), std::to_string(msg.sender_id).c_str());
