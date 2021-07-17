aedcf78f4e301183ac75bae75faa807df36408bf
Oleg Zilman <zilmano@gmail.com>
2021-07-17 18:43:17 -0500

    runmonitor to run websocket

--- runmonitor.sh
+++ runmonitor.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 roscore &
-sleep 3
+sleep 2
 rosrun distributed_mapf monitor &
 sleep 4
-rosrun distributed_mapf central_clock
+rosrun distributed_mapf central_clock &
+./src/distributed_mapf/bin/websocket 
+
 
